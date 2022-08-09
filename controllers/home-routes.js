1635a25553bc4ab63d233e97acab46f2503e62e4
Sara <sameads9@gmail.com>
2022-08-08 21:56:12 -0500

    login/logout functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,8 @@ const sequelize = require('../config/connection');
 const {Post, User, Comment} = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: [
             'id',
@@ -34,6 +36,16 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+    // if already logged in, return to homepage
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
